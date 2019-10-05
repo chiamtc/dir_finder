b2328ff94019e5dd9690bf43636030b98c743980
tat cheng chiam <chiamcheng@gmail.com>
2019-10-05 19:24:24 +1000

    update source and readme

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 - Matches regex pattern in a directory or file name because Mac search is not up on par with Windows search
 
 ## Roadmap
+- cross-env this repo
 - Testing
+- different mode (strict, loose)
 - Take args as param
 - Make it as CLI
+- Summary in CLI
+- Open Summary in html and link of each source file
+
--- finder.js
+++ finder.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const files = fs.readdirSync(`/Volumes/TC/downloads`);
 
+//source: https://stackoverflow.com/questions/520252/is-there-a-regular-expression-to-find-two-different-words-in-a-sentence
 var regex = new RegExp('(?:.*queryA.*queryB)', "gi");
 //loose
 var looseRegex = new RegExp('(?:queryA|queryB)', "gi");
